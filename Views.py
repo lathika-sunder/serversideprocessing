1dc3ddd9909592014b6f03aee0fccbf846d730a5
Lathika Sunder <lathikasunder11@gmail.com>
2022-02-20 13:52:13 +0530

    Completed

---
+++ Views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+def areacalculation(request):
+
+    context = {}
+    context[ 'area' ] = "0"
+    context[ 'l' ] = "0"
+    context[ 'b' ] = "0"
+    if request.method == 'POST' :
+        l = request.POST.get('length','0')
+        b = request.POST.get('breadth','0')
+        area = int(l) * int(b)
+        context[ 'area' ] = area
+        context[ 'l' ] = l
+        context[ 'b' ] = b
+    return render(request,'mathapp/area.html',context)
